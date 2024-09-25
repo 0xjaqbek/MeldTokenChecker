2c77b79f339fbaa3a1ce6861e0e3222bbcf66c6f
0xjaqbek <jaqbek.eth@gmail.com>
2024-09-25 15:24:36 +0200

    small fix

--- meldtokenchecker/src/App.js
+++ meldtokenchecker/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Meld Token Eligibility Checker</h1>
+        <h1>Meld Eligibility Checker</h1>
       </header>
       <main>
         <MeldTokenChecker />
