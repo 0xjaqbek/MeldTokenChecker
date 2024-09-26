22a7febc25da29f16bbb73d22e0a12c9d7e99390
0xjaqbek <jaqbek.eth@gmail.com>
2024-09-26 13:06:06 +0200

    next

--- meldtokenchecker/src/App.js
+++ meldtokenchecker/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Meld Eligibility Checker</h1>
+        <h2>Choose gate:</h2>
       </header>
       <main>
         <MeldTokenChecker />
